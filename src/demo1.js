86ed6220200cf8c889a0ab1a6c6b5f3d097760cf
Richard Snajdr <richard.snajdr@bisnode.com>
2018-02-12 18:31:39 +0100

    Lesson 2, working with array, anonym function, filter

--- src/demo1.js
+++ src/demo1.js
@@ -1,12 +1,39 @@
-function first(numberOne, secondNumber) {
-  console.log("number one", numberOne);
-  console.log("number two", secondNumber);
+function first() {
+
+  const arr1 = ['abc',  123, {attr1: 'attr1'}];
+  const arr2 = ['aaa',  111, {attr2: 'attr2'}];
+  // let i;
+
+  const maAnonFce1 = (element) => { // anaonymni funkce, je ji jedno, jak jsou pojmenovane
+    // console.log('ELEMENT 1 => ', element);
+  }
+
+  const maAnonFce2 = (element, key, attr3) => { // element, poradi prvku, cele pole
+    // console.log('ELEMENT 2 => ', element);
+    // console.log('KEY => ', key);
+    // console.log('SOME => ', attr3);
+  }
+
+  const delArr2 = arr2.filter((element) => {
+    if (element === 'aaa') {
+      return false;
+    }
+
+    return true
+  })
+
+  console.log('Delete from arr2 ', delArr2);
+
+  arr1.forEach(maAnonFce2); // predavam referenci, nevolam primo funkci
+  // maAnonFce1(); primo vola funkci
+
+  arr2.forEach((element) => console.log('ELEMENT 3 => ', element));
+
 }
 
 export function second(someText, someNumber) {
   const result = `${someText} -- ${someNumber}`;
   return result;
 }
-
 
 export default first;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import {app} from './consts';
-import First, { Second } from './demo1.js';
+import first, { second } from './demo1.js';
 
 //here routes defined
 import './routes';
 
 app.listen(3000, () => {
-  // First(1, 88);
+  first();
   console.log('ES6 application listening on port 3000!');
   // const result = Second('asdfa', 67);
   // console.log(result);
